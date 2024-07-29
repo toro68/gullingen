9d430fb3e7b1f6928377a28b73a1a11208230992
Tor Jossang <tor.jossang@gmail.com>
2024-07-29 09:48:38 +0200

    «update»

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ def snow_drift_alarm(timestamps, wind_speeds, precipitations, snow_depths, tempe
         condition2 = (wind_speeds[i] > 7 and
                       precipitations[i] >= 0.1 and
                       not np.isnan(snow_depths[i-1]) and not np.isnan(snow_depths[i]) and
-                      snow_depths[i] - snow_depths[i-1] <= -0.2 and
+                      snow_depths[i] - snow_depths[i-1] <= -1.0 and
                       not np.isnan(temperatures[i]) and temperatures[i] <= -1)
         
         if condition1 or condition2:
